5d7e05f74d8d1795d1a5bd23c8bebf21101b055b
Shubham Yewalekar <shubhamyewalekar@gmail.com>
2020-05-13 16:31:26 +0530

    Single Page Applications

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle,ListGroup ,ListGroupItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
+import {Breadcrumb, BreadcrumbItem} from 'reactstrap';
 
 function RenderDish({dish}){
 	return (
@@ -16,6 +18,7 @@ function RenderDish({dish}){
 function RenderComment({comments}){
 	const temp = [];
 	if(comments!=null){
+		console.log(comments.length);
 		for(var i=0;i<comments.length;i++){
 			temp.push(
 				<ListGroupItem key={comments[i].cid}>
@@ -34,19 +37,31 @@ function RenderComment({comments}){
 const DishDetail = (props)=> {
 
 	const dish = props.dishDetails;
+	const comments = props.comments;
 
 	if(dish != null){
 		return(
 			<div className="container">
 				<div className="row">
-					<div className="col-12 col-md-5 m-1">
-						<RenderDish dish={props.dishDetails}/>
+					<Breadcrumb>
+						<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+						<BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{dish.name}</h3>
+						<hr />
 					</div>
+				</div>
+				
+				<div className="row">	
+					<div className="col-12 col-md-5 m-1">
+						<RenderDish dish={dish}/>
+					</div>	
 
 					<div className="col-12 col-md-5 m-1">
 						<h4><strong>Comments</strong></h4>
 						<ListGroup>
-							<RenderComment comments = {dish.comments}/>
+							<RenderComment comments = {comments}/>
 						</ListGroup>
 					</div>
 				</div>
